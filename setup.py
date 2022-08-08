8ecae92278b04ab7a2eb91d535ef58c0f4a8f836
dquinnfrank <dquinnfrank@gmail.com>
2022-08-07 17:49:32 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ sys_info = {"base_directory" : current_dir}
 json.dump(sys_info, open("sys_info.json", "w"))
 
 auto_start_script = "cd {current_dir} && python driver.py".format(current_dir = current_dir)
-with open("linux_run_on_start_2.sh", "w") f:
+with open("linux_run_on_start_2.sh", "w") as f:
 	f.write(auto_start_script)
